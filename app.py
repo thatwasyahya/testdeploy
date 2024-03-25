177d695da3e6bfdfba95e5cb3d7dc5f190b53444
thatwasyahya <98318509+thatwasyahya@users.noreply.github.com>
2024-03-25 10:20:39 +0000

    tt

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import numpy as np
-import time
 import torch
 from utile import get_legal_moves, initialze_board
 
@@ -15,70 +14,31 @@ def input_seq_generator(board_stats_seq, length_seq):
     if len(board_stats_seq) >= length_seq:
         input_seq = board_stats_seq[-length_seq:]
     else:
-        input_seq = [board_stat_init]
-        for i in range(length_seq - len(board_stats_seq) - 1):
-            input_seq.append(board_stat_init)
-        for i in range(len(board_stats_seq)):
-            input_seq.append(board_stats_seq[i])
-
+        input_seq = [board_stat_init] * (length_seq - len(board_stats_seq))
+        input_seq.extend(board_stats_seq)
+            
     return input_seq
 
 def find_best_move(move1_prob, legal_moves):
     best_move = legal_moves[0]
     max_score = move1_prob[legal_moves[0][0], legal_moves[0][1]]
     
-    for i in range(len(legal_moves)):
-        if move1_prob[legal_moves[i][0], legal_moves[i][1]] > max_score:
-            max_score = move1_prob[legal_moves[i][0], legal_moves[i][1]]
-            best_move = legal_moves[i]
+    for move in legal_moves:
+        if move1_prob[move[0], move[1]] > max_score:
+            max_score = move1_prob[move[0], move[1]]
+            best_move = move
     return best_move
 
 class ReversiGrid:
     def __init__(self):
         self.board = [[0 for _ in range(8)] for _ in range(8)]
         self.current_player = -1
-        self.create_board()
-        self.place_initial_pieces()
-
-    def create_board(self):
-        for row in range(8):
-            for col in range(8):
-                cell = Button(background_color=(1, 1.8, 1, 1.8))  # Light green background
-                cell.bind(on_press=self.make_move) # type: ignore
-                
-                if self.board[row][col] == -1:
-                    cell.background_normal = 'black_circle.png'
-                elif self.board[row][col] == 1:
-                    cell.background_normal = 'white_circle.png'
-                
-                self.add_widget(cell)
-
-    def place_initial_pieces(self):
-        self.board[3][3] = 1
-        self.board[4][4] = 1
-        self.board[3][4] = -1
-        self.board[4][3] = -1
-        for i, child in enumerate(reversed(self.children)):
-            row, col = self.get_coords(child)
-            if (row, col) in [(3, 3), (4, 4)]:
-                child.background_normal = 'white_circle.png'
-            elif (row, col) in [(3, 4), (4, 3)]:
-                child.background_normal = 'black_circle.png'
-
-    
-    def get_coords(self, instance):
-        index = self.children.index(instance)
-        row = index // self.cols
-        col = index % self.cols
-        return row, col
 
     def is_valid_move(self, row, col):
         if self.board[row][col] != 0:
             return False
 
         directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-        valid_move = False
-
         for dx, dy in directions:
             r, c = row + dx, col + dy
             to_flip = []
@@ -87,31 +47,21 @@ class ReversiGrid:
                 r += dx
                 c += dy
             if 0 <= r < 8 and 0 <= c < 8 and self.board[r][c] == self.current_player and to_flip:
-                valid_move = True
-                break
+                return True
 
-        return valid_move
+        return False
 
     def make_move(self, row, col):
         if self.is_valid_move(row, col):
             self.board[row][col] = self.current_player
             self.current_player = -1 if self.current_player == 1 else 1
-            black_count, white_count = self.count_pieces()
 
             if not any(self.is_valid_move(row, col) for row in range(8) for col in range(8)):
                 black_count, white_count = self.count_pieces()
-                winner = "Black" if black_count > white_count else "White" if white_count > black_count else "Draw"
+                winner = "Black" if black_count > white_count else "White" if white_count < black_count else "Draw"
                 return {"success": True, "winner": winner}
 
         return {"success": True}
-
-    def update_board(self):
-        for i, child in enumerate(reversed(self.children)):
-            row, col = self.get_coords(child)
-            if self.board[row][col] == -1:
-                child.background_normal = 'black_circle.png'  
-            elif self.board[row][col] == 1:
-                child.background_normal = 'white_circle.png'
 
     def make_one_move(self, player_disc, player):
         if ((self.current_player == -1 and player_disc == 'Black') or (self.current_player == 1 and player_disc == 'White')):
@@ -141,19 +91,6 @@ class ReversiGrid:
         if len(legal_moves) > 0:
             best_move = find_best_move(move1_prob, legal_moves)
             return best_move
-
-    def flip_pieces(self, row, col):
-        directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-        for dx, dy in directions:
-            r, c = row + dx, col + dy
-            to_flip = []
-            while 0 <= r < 8 and 0 <= c < 8 and self.board[r][c] != 0 and self.board[r][c] != self.current_player:
-                to_flip.append((r, c))
-                r += dx
-                c += dy
-            if 0 <= r < 8 and 0 <= c < 8 and self.board[r][c] == self.current_player:
-                for r, c in to_flip:
-                    self.board[r][c] = self.current_player
 
     def count_pieces(self):
         black_count = sum(row.count(-1) for row in self.board)
@@ -186,7 +123,7 @@ def make_move():
 
     return response
 
-@app.route('/api/make_one_move', methods=['POST'])
+@app.route('/make_one_move', methods=['POST'])
 def make_one_move():
     data = request.get_json()
     difficulty = data['difficulty']
@@ -204,4 +141,3 @@ def make_one_move():
         response.headers.add('winner', winner)
 
     return response
-
