5e877daab2b38b93ca1911ac7153c93477a006cf
thatwasyahya <98318509+thatwasyahya@users.noreply.github.com>
2024-03-26 12:48:19 +0000

    ia

--- app.py
+++ app.py
@@ -1,9 +1,56 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import numpy as np
+import torch
 from utile import get_legal_moves, initialze_board
 
+BOARD_SIZE=8
 app = Flask(__name__)
 CORS(app)
+
+
+def input_seq_generator(board_stats_seq,length_seq):
+    
+    board_stat_init=initialze_board()
+
+    if len(board_stats_seq) >= length_seq:
+        input_seq=board_stats_seq[-length_seq:]
+    else:
+        input_seq=[board_stat_init]
+        #Padding starting board state before first index of sequence
+        for i in range(length_seq-len(board_stats_seq)-1):
+            input_seq.append(board_stat_init)
+        #adding the inital of game as the end of sequence sample
+        for i in range(len(board_stats_seq)):
+            input_seq.append(board_stats_seq[i])
+            
+    return input_seq
+
+def find_best_move(move1_prob,legal_moves):
+    """
+    Finds the best move based on the provided move probabilities and legal moves.
+
+    Parameters:
+    - move1_prob (numpy.ndarray): 2D array representing the probabilities of moves.
+    - legal_moves (list): List of legal moves.
+
+    Returns:
+    - tuple: The best move coordinates (row, column).
+    """
+
+    # Initialize the best move with the first legal move
+    best_move=legal_moves[0]
+    
+    # Initialize the maximum score with the probability of the first legal move
+    max_score=move1_prob[legal_moves[0][0],legal_moves[0][1]]
+    
+    # Iterate through all legal moves to find the one with the maximum probability
+    for i in range(len(legal_moves)):
+        # Update the best move if the current move has a higher probability
+        if move1_prob[legal_moves[i][0],legal_moves[i][1]]>max_score:
+            max_score=move1_prob[legal_moves[i][0],legal_moves[i][1]]
+            best_move=legal_moves[i]
+    return best_move
 
 class ReversiGrid:
     def __init__(self):
@@ -30,6 +77,8 @@ class ReversiGrid:
                 r += dx
                 c += dy
             if 0 <= r < 8 and 0 <= c < 8 and self.board[r][c] == self.current_player and to_flip:
+                for r, c in to_flip:
+                    self.board[r][c] = self.current_player
                 return True
 
         return False
@@ -47,8 +96,43 @@ class ReversiGrid:
 
         return {"success": True}
 
-    def make_one_move(self, player_disc, player):
-        return -1, -1
+    def make_one_move(self, playerDisc, player): # player = difficulty (type of AI)
+    # player: model description
+    # board_stat: current 8x8 board status
+    # turn: 1 or -1 - black or white turn
+        # if current move is for player, skip
+        if ((self.current_player == -1 and playerDisc == 'Black') or (self.current_player == 1 and playerDisc == 'White')):
+            return -1, -1
+        device = torch.device("cpu")
+
+        conf = {}
+        if (player == 'Easy'):
+            conf['player']= 'server\\api\\Easy.pt'
+        elif (player == 'Medium'):
+            conf['player']= 'server\\api\\Medium.pt'
+        elif (player == 'Hard'):
+            conf['player']= 'Hard.pt'
+        
+        model = torch.load(conf['player'],map_location=torch.device('cpu'))
+        model.eval()
+        input_seq_boards = input_seq_generator(self.board,model.len_inpout_seq)
+        
+        
+        #if black is the current player the board should be multiplay by -1
+        if (self.current_player == -1):
+            model_input=np.array([input_seq_boards])*-1
+        else:
+            model_input = np.array([input_seq_boards])
+        move1_prob = model(torch.tensor(model_input).float().to(device))
+        move1_prob = move1_prob.cpu().detach().numpy().reshape(8,8)
+        legal_moves = get_legal_moves(self.board, self.current_player)
+        if len(legal_moves) > 0:
+            best_move = find_best_move(move1_prob,legal_moves)
+            if (self.current_player == -1):
+                print(f"Black: {best_move} < from possible move {legal_moves}")
+            else:
+                print(f"White: {best_move} < from possible move {legal_moves}")
+            return best_move
     
     def count_pieces(self):
         black_count = sum(row.count(-1) for row in self.board)
